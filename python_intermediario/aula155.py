abd174d7319a2940789f00be92728e80f8761216
Luiz <lgsiqueiracosta@hotmail.com>
2024-07-01 22:02:23 -0300

    aula 155 e 156 sobre modulos

---
+++ python_intermediario/aula155.py
@@ -0,0 +1,17 @@
+# Modularização - Entendendo os seus próprios módulos Python
+# O primeiro módulo executado chama-se __main__
+# Você pode importar outro módulo inteiro ou parte do módulo
+# O python conhece a pasta onde o __main__ está e as pastas
+# abaixo dele.
+# Ele não reconhece pastas e módulos acima do __main__ por
+# padrão
+# O python conhece todos os módulos e pacotes presentes
+# nos caminhos de sys.path
+import modulo155
+from modulo155 import nome, soma
+
+print(modulo155.nome)
+print(nome)
+
+print(soma(2,3))
+print(modulo155.soma(3,5))
