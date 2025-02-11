19ea23e257a85522f7430dbbd0a75a5a4eb9c99e
Luiz <lgsiqueiracosta@hotmail.com>
2025-02-10 22:12:01 -0300

    Aulas sobre Log, Mixin (Abstrações)

--- introducao_a_programacao_orientada_a_objetos_pooo_classes/aula221_supeclass.py
+++ introducao_a_programacao_orientada_a_objetos_pooo_classes/aula221_supeclass.py
@@ -3,3 +3,32 @@
 #   -> super class, base class, parent class
 # Classes filhas (Cliente)
 #   -> sub class, child class, derived class
+# class MinhaString(str):
+#     def upper(self):
+#         print('Chamou Upper')
+#         return super().upper()
+
+# string = MinhaString('Luiz')
+# print(string.upper())
+
+class A:
+    atributo_a = 'valor a'
+    def metodo (self):
+        print('A')
+
+
+class B(A):
+    atributo_b = 'valor b'
+    def metodo (self):
+        print('B')
+
+
+class C(B):
+    atributo_c = 'valor c'
+    def metodo (self):
+        print('C')
+
+c = C()
+print(c.atributo_a)
+print(c.atributo_b)
+print(c.atributo_c)
