185af097898f82194318a79c5a39aa352c02cb20
Luiz <lgsiqueiracosta@hotmail.com>
2025-02-11 22:35:17 -0300

    Aulas sobre o Log e classes abstratas

--- aula225/log.py
+++ aula225/log.py
@@ -1,4 +1,8 @@
 # Abstração
+from pathlib import Path
+
+LOG_FILE = Path(__file__).parent / 'log.txt'
+
 class Log:
     def _log(self, msg):
         raise NotImplementedError('Implemente o método log')
@@ -11,7 +15,11 @@ class Log:
 
 class LogFileMixin(Log):
     def _log(self, msg):
-        print(msg)
+        msg_formatada = f'{msg} ({self.__class__.__name__})'
+        print('Salvando no log: ', msg_formatada)
+        with open(LOG_FILE, 'a') as arquivo:
+            arquivo.write(msg_formatada)
+            arquivo.write('\n')
 
 
 class LogPrintMixin(Log):
@@ -19,6 +27,10 @@ class LogPrintMixin(Log):
         print(f'{msg} ({self.__class__.__name__})')
 
 if __name__ == '__main__':
-    l = LogFileMixin()
-    l.log_error ('Qualquer coisa')
-    l.log_success('Que legal')
+    lp = LogPrintMixin()
+    lp.log_error ('Qualquer coisa')
+    lp.log_success('Que legal')
+    lf = LogFileMixin()
+    lf.log_error ('Qualquer coisa')
+    lf.log_success('Que legal')
+    
